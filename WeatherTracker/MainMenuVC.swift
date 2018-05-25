9f2687ddcf611941d371a3599dfbcbe1e2cf08f2
MediBoss <mediassumani49@gmail.com>
2018-05-25 00:32:15 -0400

    adding corelocation

--- WeatherTracker/MainMenuVC.swift
+++ WeatherTracker/MainMenuVC.swift
@@ -17,17 +17,35 @@ class MainMenuVC: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     
     let apiKey: String = Constants.apiKey
-    let coordinate: (latitude: Double, longitude: Double) = (37.8267,-122.4233)
+    var coordinate: (latitude: Double, longitude: Double)
+    var locManager = CLLocationManager()
+    var currentLocation: CLLocation!
+    
+   func checkAuthorization() -> (latitude: Double, longitude: Double){
+        locManager.requestWhenInUseAuthorization()
+    
+        
+        if( CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
+            CLLocationManager.authorizationStatus() ==  .authorizedAlways){
+            currentLocation = locManager.location
+        }
+        
+            // return a tuple that contains the coordinates
+        return(currentLocation.coordinate.latitude,currentLocation.coordinate.longitude)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.coordinate = checkAuthorization()
+        
         let forecastService = ForecastService(apiKey: self.apiKey)
         forecastService.getForecast(latitude: coordinate.latitude, longitude: coordinate.longitude) { (currentWeather) in
             if let currentWeather = currentWeather {
-                guard let summary = self.summaryLabel.text, let temperature = self.summaryLabel.text, let location = self.locationLabel.text else{return}
+                guard var summary = self.summaryLabel.text, var temperature = self.summaryLabel.text, var location = self.locationLabel.text else{return}
                 // MUST GET BACK TO THE MAIN QUEUE
                 DispatchQueue.main.async {
+                    
                     
                 }
             }
