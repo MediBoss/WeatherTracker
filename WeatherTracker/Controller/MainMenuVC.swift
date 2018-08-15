652e9a53b1d73402ddd8e92b0092c40e71cfb2f5
MediBoss <mediassumani49@gmail.com>
2018-08-14 22:49:33 -0400

    fixed bug and added automatic user location

--- WeatherTracker/Controller/MainMenuVC.swift
+++ WeatherTracker/Controller/MainMenuVC.swift
@@ -7,32 +7,54 @@
 //
 
 import UIKit
+import Foundation
 import CoreLocation
 
 final class MainMenuVC: UIViewController, CLLocationManagerDelegate {
     
     // -MARK: @IBOULETS
+    @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
     
     // -MARK: PROPERTIES
     
-    var locationManager = CLLocationManager()
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         getUserCoordinates()
-
+        self.lookUpCurrentLocation { (currentUserPlace) in
+            guard let city = currentUserPlace?.locality else {return}
+            self.cityLabel.text = city
+        }
     }
     
     // -MARK: CLASS METHODS
     
     
-    /*
-     This function requests permission to get user's location, calculates the location's
-     accuracy, and updates the location frequently.
-     */
+        // From Apple's documentation
+        // This function gets the informtaion about the current location(address,city, state)
+    fileprivate func lookUpCurrentLocation(completionHandler: @escaping (CLPlacemark?)-> Void ) {
+
+        if let lastLocation = self.locationManager.location {
+            let geocoder = CLGeocoder()
+            geocoder.reverseGeocodeLocation(lastLocation, completionHandler: { (placemarks, error) in
+                if error == nil {
+                    let firstLocation = placemarks?[0]
+                    completionHandler(firstLocation)
+                }else {
+                    completionHandler(nil)
+                }
+            })
+        }
+        else {
+            completionHandler(nil)
+        }
+    }
+    
+        //This function requests permission to get user's location
     fileprivate func getUserCoordinates(){
         
         self.locationManager.requestAlwaysAuthorization()
@@ -43,19 +65,16 @@ final class MainMenuVC: UIViewController, CLLocationManagerDelegate {
         }
     }
 
-    /*
-        This Function gets the current user location from the ios device and updates
-        The coordinates with realtime and recent location.
-     */
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+    
+        //This Function gets the current user location from the device and makes the call to the api
+    internal func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
         
         guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else {return}
         let forecastService = ForecastService(locValue.latitude, locValue.longitude)
         forecastService.getForecast { (currentWeather) in
-            // UPDATING THE UI
             DispatchQueue.main.async {
+                        // UPDATING Temperature's and Summary's Labels
                 guard let weatherSummary = currentWeather.summary, let weatherTemperature = currentWeather.temperature else{return}
-                print(weatherTemperature)
                 self.summaryLabel.text = weatherSummary
                 self.temperatureLabel.text = weatherTemperature.convertToInt().convertToString() + "°"
             }
